c76aea2456b5a24bb449caf9b272ec7fcc3f69ac
abbey <abbeyezekiel100@gmail.com>
2025-06-08 11:23:22 +0100

    RESTful Api automation test

---
+++ src/test/java/org/apiautomation/Basics.java
@@ -0,0 +1,40 @@
+package org.apiautomation;
+
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        //Validate uf Add place API is working as expected
+        // Rest Assured is a Java library that makes it easy to test RESTful web services. below are methods to validate the API response
+        // given - all input details
+        // when - Submit the API - resource,http methods, etc.
+        // then - validate the response
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": -38.383494,\n" +
+                        "    \"lng\": 33.427362\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"Godwitus Global\",\n" +
+                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://google.com\",\n" +
+                        "  \"language\": \"French-IN\"\n" +
+                        "}\n")
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
+
+
+    }
+}
